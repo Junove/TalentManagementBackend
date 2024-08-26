b8b79aac6172571bc72ccc60072593b2414c2cda
hattvr <hatterhg@gmail.com>
2024-08-26 14:31:26 -0400

    update: API implementation using entity objects

--- src/main/java/com/example/talent_api/controllers/CandidateController.java
+++ src/main/java/com/example/talent_api/controllers/CandidateController.java
@@ -1,5 +1,8 @@
 package com.example.talent_api.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,36 +12,54 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.talent_api.entity.Candidate;
+import com.example.talent_api.repository.CandidateRepository;
 
 @CrossOrigin
 @RestController
 public class CandidateController {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @GetMapping("/candidates")
-    public String getAll() {
-        return "All Candidates";
+    public Iterable<Candidate> getAll() {
+        return candidateRepository.findAll();
     }
 
     // Read Operation
     @GetMapping("/candidates/{id}")
-    public String getCandidateById(@PathVariable("id") Long id) {
-        return "Candidate with ID: " + id;
+    public Optional<Candidate> getCandidateById(@PathVariable("id") Long id) {
+        return candidateRepository.findById(id);
     }
 
     // Create Operation
     @PostMapping("/candidates")
-    public String addCandidate(@RequestBody String candidate) {
-        return "Candidate added";
+    public Candidate addCandidate(@RequestBody Candidate candidate) {
+        return candidateRepository.save(candidate);
     }
 
     // Update Operation
     @PutMapping("/candidates/{id}")
-    public String updateCandidate(@PathVariable Long id, @RequestBody String candidate) {
-        return "Candidate updated";
+    public Candidate updateCandidate(@PathVariable Long id, @RequestBody Candidate candidate) {
+        Optional<Candidate> candidateOptional = candidateRepository.findById(id);
+        if (candidateOptional.isPresent()) {
+            Candidate candidateToUpdate = candidateOptional.get();
+            candidateToUpdate.setUser(candidate.getUser());
+            candidateToUpdate.setFullName(candidate.getFullName());
+            candidateToUpdate.setEmail(candidate.getEmail());
+            candidateToUpdate.setAddress(candidate.getAddress());
+            candidateToUpdate.setPhone(candidate.getPhone());
+            candidateToUpdate.setResume(candidate.getResume());
+
+            return candidateRepository.save(candidateToUpdate);
+        } else {
+            return null;
+        }
     }
 
     // Delete Operation
     @DeleteMapping("/candidates/{id}")
-    public String deleteCandidate(@PathVariable Long id) {
-        return "Candidate deleted";
+    public void deleteCandidate(@PathVariable Long id) {
+        candidateRepository.deleteById(id);
     }
 }
