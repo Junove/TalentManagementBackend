a869ebc6f3a6f05bc9c36b6dc26c553e4e355b9e
Xiyu-Serena-Wan <xiyuwanbjtu@gmail.com>
2024-08-26 19:28:06 +0000

    test repo use

--- src/main/java/com/example/talent_api/repository/UserRepository.java
+++ src/main/java/com/example/talent_api/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.talent_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.talent_api.entity.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{
-
+    public Optional<User> findUserById(long id);
     
 } 
