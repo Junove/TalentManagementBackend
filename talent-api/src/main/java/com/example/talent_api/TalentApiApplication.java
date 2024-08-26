fc266c622fde4690b869fee38864fcd6feff14f6
akshitha-singathi-0 <as283@njit.edu>
2024-08-26 13:52:18 +0000

    initial commit

---
+++ talent-api/src/main/java/com/example/talent_api/TalentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.talent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentApiApplication.class, args);
+	}
+
+}
