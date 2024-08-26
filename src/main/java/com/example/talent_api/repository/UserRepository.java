34020b6f41bdd5763a4589fbe040408c65a95be7
Xiyu-Serena-Wan <xiyuwanbjtu@gmail.com>
2024-08-26 16:09:30 +0000

    user entity restructure

--- src/main/java/com/example/talent_api/repository/UserRepository.java
+++ src/main/java/com/example/talent_api/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.talent_api.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.talent_api.domain.User;
+import com.example.talent_api.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 
