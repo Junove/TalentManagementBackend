5e7a6e5a7e7c8c20f967f85f28cff6c27c3b132b
Junove <nathaniel.endick@gmail.com>
2024-08-26 18:10:36 +0000

    bug fix

--- src/test/java/com/example/talent_api/entity/CandidateTest.java
+++ src/test/java/com/example/talent_api/entity/CandidateTest.java
@@ -1,4 +1,4 @@
-package com.example.talent_api.repository;
+package com.example.talent_api.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
