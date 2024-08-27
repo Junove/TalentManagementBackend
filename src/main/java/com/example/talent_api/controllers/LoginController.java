24daa0ca48766088c57df6d2e823102aa4c502ed
akshitha-singathi-0 <as283@njit.edu>
2024-08-27 13:43:13 +0000

    login fully functional!

--- src/main/java/com/example/talent_api/controllers/LoginController.java
+++ src/main/java/com/example/talent_api/controllers/LoginController.java
@@ -4,9 +4,12 @@ import com.example.talent_api.entity.User;
 import com.example.talent_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
 
