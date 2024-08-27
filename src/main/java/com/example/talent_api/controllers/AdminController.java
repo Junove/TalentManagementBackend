414e15b6913362a7ddfdb8acd8e5a30ae56bb086
shivakalam <shivakalam1016@gmail.com>
2024-08-27 15:39:57 +0000

    Feat:  admin CRUD Operations

---
+++ src/main/java/com/example/talent_api/controllers/AdminController.java
@@ -0,0 +1,76 @@
+package com.example.talent_api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.talent_api.entity.Admin;
+import com.example.talent_api.repository.AdminRepository;
+
+@RestController
+@RequestMapping("/admins")
+public class AdminController {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Admin>> getAllAdmins() {
+        return new ResponseEntity<>((List<Admin>) adminRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Admin> getAdminById(@PathVariable Long id) {
+        Admin admin = adminRepository.findById(id).orElse(null);
+        if (admin != null) {
+            return new ResponseEntity<>(admin, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+    try {
+        Admin savedAdmin = adminRepository.save(admin);
+        return new ResponseEntity<>(savedAdmin, HttpStatus.CREATED);
+    } catch (Exception e) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    }
+
+
+    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Admin> updateAdmin(@PathVariable Long id, @RequestBody Admin adminDetails) {
+        Admin admin = adminRepository.findById(id).orElse(null);
+        if (admin != null) {
+            admin.setName(adminDetails.getName());
+            admin.setEmail(adminDetails.getEmail());
+            return new ResponseEntity<>(adminRepository.save(admin), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAdmin(@PathVariable Long id) {
+        if (adminRepository.existsById(id)) {
+            adminRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
